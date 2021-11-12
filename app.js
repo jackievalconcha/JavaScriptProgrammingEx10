af23a270dc4244e67a18c5e5b9e9b1af26c4a903
jackievalconcha <90628063+jackievalconcha@users.noreply.github.com>
2021-11-12 16:57:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,10 @@
  * Adding HTML to the DOM
  * Jackie Valconcha
  */
+
+//Create a div
+let myDiv = document.createElement('div');
+
+//Set Attributes
+myDiv.setAttribute('id','newDiv');
+myDiv.setAttribute('class','box');
